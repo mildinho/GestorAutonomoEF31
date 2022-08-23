85fd27007bc6fdfbf0978b43852d4672bf3f2538
Fernando Casagrande <mildinho@gmail.com>
2022-08-23 06:19:40 -0300

    Inicio da Criação do Camada de Serviço

---
+++ GestorAutonomo/Servico/GenericoServico.cs
@@ -0,0 +1,42 @@
+﻿using GestorAutonomo.Data;
+using GestorAutonomo.Servico.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorAutonomo.Servico
+{
+    public class GenericoServico<Tabela> : IGenericoServico<Tabela> where Tabela : class
+    {
+        private readonly GestorAutonomoContext _context;
+
+
+        public GenericoServico(GestorAutonomoContext context)
+        {
+            _context = context;
+
+        }
+
+
+        public virtual async Task<Tabela> SelecionarPorCodigoAsync(Guid Id)
+        {
+            return await _context.Set<Tabela>().FindAsync(Id);
+
+        }
+
+
+        public virtual async Task<IEnumerable<Tabela>> ListarTodosRegistrosAsync(Guid EmpresaId)
+        {
+            IQueryable<Tabela> objConsulta = _context.Set<Tabela>().AsQueryable();
+
+            IQueryable<Tabela> objConsulta2 = objConsulta
+            .Where(e => <objConsulta>.EmpresaId == EmpresaId);
+            //_context.Entry<Tabela>(tabela).Property("Data_Cadastro").IsModified = false;
+
+            return await objConsulta.ToListAsync();
+
+        }
+    }
+}
---
+++ GestorAutonomo/Servico/Interface/IGenericoServico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorAutonomo.Servico.Interface
+{
+    public interface IGenericoServico<Tabela> where Tabela : class
+    {
+
+        Task<Tabela> SelecionarPorCodigoAsync(Guid Id);
+        Task<IEnumerable<Tabela>> ListarTodosRegistrosAsync(Guid EmpresaId);
+
+    }
+}
